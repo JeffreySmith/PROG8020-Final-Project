1c6f453d453dfce755d585fa593d3a5df58cfb88
Jeffrey Smith <jeffrey.smith7@gmail.com>
2023-08-01 22:09:15 -0400

    Slight touch to css, got form submitting image and server recognizing it to work

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 "use strict";
 const test = require('./htmlTemplate.js');
 const express = require("express");
+const upload = require('express-fileupload');
 const myApp = new express();
 const path = require("path");
 const port = 8080;
-
+const mongoose = require("mongoose");
 const session = require("express-session");
+
+
 myApp.use(session({
     secret :"66b40822-73c6-50c4-a5d7-8fd3aa19152a",
     resave:false,
@@ -13,6 +16,7 @@ myApp.use(session({
 }));
 
 const { check, validationResult } = require('express-validator');
+const { Z_ASCII } = require('zlib');
 
 /*let template = `<!DOCTYPE html>
 <html lang="en">
@@ -32,7 +36,7 @@ myApp.set("view engine", "ejs");
 myApp.use(express.urlencoded({ extended: true }));
 myApp.set("views",path.join(__dirname, "views"));
 myApp.use(express.static(__dirname + "/public"));
-
+myApp.use(upload());
 const pages = [];
 
 myApp.get("/",(req,res)=>{
@@ -61,6 +65,14 @@ myApp.post("/edit",[check("htmlcontent").notEmpty(),check("pagename").notEmpty()
     let html=req.body.htmlcontent;
     let title = req.body.articleTitle;
     let pageName = req.body.pagename;
+    
+    if(req.files){
+        console.log(req.files);
+        let image = req.files.Image;
+        let imageName = image.name;
+        let 
+    }
+    
     console.log(pageName)
     pageName = pageName.replaceAll(' ','-');
     
