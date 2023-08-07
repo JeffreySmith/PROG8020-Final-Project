a7d217bff94258f51d6f79dda0fd33b399fa2afa
Jeffrey Smith <jeffrey.smith7@gmail.com>
2023-08-07 14:24:30 -0400

    Cleanup

--- index.js
+++ index.js
@@ -165,21 +165,7 @@ myApp.get("/edit/:name/",(req,res)=>{
     else{
         res.redirect('/login');
     }
-    /*
-    let filter = pages.filter(x=>x.route.toLowerCase() == name.toLowerCase());
-    console.log(`There are ${filter.length} elements in the filter for ${name}`);
-    if(filter.length===1 && req.session.admin!=undefined && req.session.admin){
-        let values = {
-            html:filter[0].content,
-            title:filter[0].name,
-            pageName:filter[0].route
-        };
-        console.log(values);
-        res.render('edit',{values});
-    }
-    else{
-        res.redirect('/dashboard');
-    }*/
+    
 });
 myApp.get("/delete/:name/",(req,res)=>{
     if(req.session.admin==undefined){
@@ -201,31 +187,13 @@ myApp.get("/delete/:name/",(req,res)=>{
             res.render('delete',{confirmation});
         }
         else{
+            let notFound = name;
+            res.render('delete',{notFound});
             console.log("Page not found for editing");
-            res.redirect('/dashboard');
         }
         
     });
-    /*let filter = pages.filter(x=>x.route.toLowerCase() == name.toLowerCase());
-    if (filter.length===1 && req.session.admin){
-        let index = pages.findIndex(x=>  x.route.toLowerCase() === name.toLowerCase());
-        console.log(`index is: ${index}`);
-        pages = [
-            ...pages.slice(0,index),
-            ...pages.slice(index+1)
-        ];
-        console.log(`Deleted page /${name} successfully`);
-        let confirmation = {
-            confirm:true,
-            name:name
-        }
-        res.render('delete',{confirmation});
-    }
-    else{
-        res.redirect('/dashboard');
-    }*/
-    
-});
+ });
 
 myApp.get("/:name/",(req,res)=>{
     let name = req.params.name;
@@ -236,13 +204,9 @@ myApp.get("/:name/",(req,res)=>{
     //async problems led me to have to structure things this way. That was an incredibly frustrating bug
     //to figure out. My functions would *sometimes* succeed, then other times fail.
     Page.find({}).then((navPage)=>{
-        //console.log(`return from mongodb: ${navPage}`);
         for(let p of navPage){
-            //console.log(`Running for: ${p}`);
             pages.push(p.route);
-            
         }
-        //console.log(`eia,srntietnihar${navPage[0].route}`);
         
         Page.findOne({route:name.toLowerCase()}).then((page)=>{
         
@@ -270,31 +234,10 @@ myApp.get("/:name/",(req,res)=>{
     }).catch((error)=>{
         console.log(`Error:${error}`);
     });
-
-    
-    //Every one of these 'filter' variables needs to eventually become a call to the db
-    
-    
-    /*let filter = pages.filter(x=> x.route.toLowerCase() == name.toLowerCase());
-    if(filter.length === 1){
-        console.log("I'm running this for: "+"/"+name);
-        let nav = createNav(pages);
-        let html = createHTMLPage(filter[0].name,filter[0].name,filter[0].content,filter[0].image,nav);
-        res.send(html);
-    }
-    else{
-        console.log(req.url)   
-        if(pages.findIndex(x=>x.route.toLowerCase()==="home")!=-1){
-            res.redirect('/home');
-        }
-        else{
-            res.redirect('/login');
-        }
-    }*/
 });
 
+//check to make sure that the hidden 'route' actually exists, just in case someone tried to delete it
 myApp.post("/edit",(req,res)=>{
-    const expressErrors = validationResult(req);
     let html=req.body.htmlcontent;
     let title = req.body.articleTitle;
     let image = "";
@@ -307,24 +250,8 @@ myApp.post("/edit",(req,res)=>{
     }
     if(req.files){
         image = req.files.Image;
-        
-        //*TODO*Check for valid filetype here, but allow none if they don't want to change the photo here
         imageName=checkImage(image,errors);
     }
-
-    
-    
-    if(!expressErrors.isEmpty()){
-        for(let err of expressErrors.array()){
-            console.log(`Express errors: ${err.msg}`);
-        }
-        errors.push("You must supply a page name");
-    }
-    
-
-    
-    //console.log(`PageName:${pageName}`);
-    //let filter = pages.filter(x=>x.route === pageName);
     
     let values={};
     if(errors.length===0){
@@ -334,7 +261,8 @@ myApp.post("/edit",(req,res)=>{
                 imageName = p.imageName;
             }
             if(p){
-
+                //I couldn't figure out how to find something correctly using a different column, so I ended
+                //up having to nest calls to the db
                 Page.findByIdAndUpdate({_id:p._id}).then((page)=>{
                     page.html = html,
                     page.title = title,
@@ -357,39 +285,6 @@ myApp.post("/edit",(req,res)=>{
         };
         res.render('edit',{values,errors});
     }
-    
-
-    /*if(errors.length===0 && filter.length===1){
-        
-        if(imageName!=""){
-            filter[0].image=imageName;
-        }
-        else{
-            filter[0].image=filter[0].image;
-        }
-        console.log(filter[0].route)
-
-        filter[0].name=title;
-        filter[0].content=html;
-       
-
-        let confirmation = {confirm:true};
-        res.render("edit",{confirmation});
-    }
-   else if( filter.length==1 && errors.length>0){
-        console.log(`filter is: ${filter}`);
-        values = {
-            html:filter[0].content,
-            title:filter[0].name,
-            pageName:filter[0].route
-        };
-        res.render("edit",{errors,values});
-    }
-    else{
-        
-        console.log("Running else for edit.post");
-        res.render("edit",{errors,values});
-    }*/
 });
 myApp.post("/add/",[check("pagename").notEmpty()],(req,res)=>{
     const expressErrors = validationResult(req);
@@ -407,7 +302,6 @@ myApp.post("/add/",[check("pagename").notEmpty()],(req,res)=>{
     if(!req.session.admin){
         errors.push("You don't have permission to do this. Please login");
     }
-    //Eventually don't let it put this value in on error. It should not insert an invalid page route
     if(pageName.includes('/')){
         errors.push("Please don't put a '/' in the page name");
     }
@@ -472,39 +366,6 @@ myApp.post("/add/",[check("pagename").notEmpty()],(req,res)=>{
     }).catch((error)=>{
         console.log(`Error:${error}`);
     });
-
-    //eventually a call to the db
-    /*let filter = pages.filter(x=>x.route === pageName);
-    console.log(`Filter length: ${filter.length}`);
-    if(errors.length===0){
-        //This means a page with that name does not yet exist
-        if(filter.length===0){
-            let page = {
-                name:title,
-                route:pageName,
-                content:html,
-                image:imageName
-            };
-            pages.push(page);
-            console.log(pageName);
-        }
-        
-    }   
-    let values={};
-    console.log(pages);
-    if(errors.length>0){
-        values = {
-            html:html,
-            title:title,
-            pageName:pageName
-        };
-        res.render("add",{errors,values});
-    }
-    else{
-        let confirmation = {confirm:true};
-        //res.render("add",{values});
-        res.render("add",{confirmation});
-    }*/
 });
 myApp.post('/login',(req,res)=>{
     if(req.session.admin==undefined){
@@ -527,8 +388,7 @@ myApp.post('/login',(req,res)=>{
     }).catch((err)=>{
         console.log(`Something went wrong: ${err}`);
     });
-    
-    
 });
+
 myApp.listen(port);
-console.log("Server running");
+console.log("Server running: http://localhost:8080");
